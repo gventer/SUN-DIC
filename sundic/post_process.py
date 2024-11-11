fde890e0eb56d6f4427771824a82485c228baf6f
EMStockhall <23648341@sun.ac.za>
2024-11-11 11:52:09 +0200

    test update of plotDispContour

--- sundic/post_process.py
+++ sundic/post_process.py
@@ -270,6 +270,9 @@ def plotDispContour(resultsFile, imgPair, dispComp=DispComp.DISP_MAG,
     settings = sdset.Settings.fromMsgPackDict(setDict)
     inFile.close()
 
+    # Create figure object
+    fig, ax = plt.subplots()
+
     # Read the image to plot on and plot
     if plotImage:
         imgSet = sdic._getImageList_(settings.ImageFolder)
@@ -278,15 +281,13 @@ def plotDispContour(resultsFile, imgPair, dispComp=DispComp.DISP_MAG,
         else:
             imgPair = imgPair + 1
         img = cv2.imread(imgSet[imgPair], cv2.IMREAD_GRAYSCALE)
-        plt.imshow(img, zorder=1, cmap='gray', vmin=0, vmax=255)
+        ax.imshow(img, zorder=1, cmap='gray', vmin=0, vmax=255)
 
     # Setup the contour plot and plot on top of the image
-    plt.contourf(X, Y, Z, alpha=alpha, zorder=2)
-    cmap = plt.get_cmap('jet')
-    plt.set_cmap(cmap)
-    plt.xlabel('x (pixels)')
-    plt.ylabel('y (pixels)')
-    plt.colorbar()
+    contour = ax.contourf(X, Y, Z, alpha=alpha, zorder=2, cmap='jet')
+    ax.xlabel('x (pixels)')
+    ax.ylabel('y (pixels)')
+    fig.colorbar(contour, ax = ax)
 
     # Show and or save the plot
     if showPlot:
@@ -294,7 +295,7 @@ def plotDispContour(resultsFile, imgPair, dispComp=DispComp.DISP_MAG,
     if fileName:
         plt.savefig(fileName)
 
-    return
+    return fig
 
 
 # --------------------------------------------------------------------------------------------
