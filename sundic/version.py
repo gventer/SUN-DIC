6df6cdcd26e3375e8f344a98a2749d60210efad4
Gerhard Venter <gventer@sun.ac.za>
2025-01-22 17:54:18 +0200

    Update pypi.yml

--- sundic/version.py
+++ sundic/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 # 0.0.1 - Initial release
 # 0.0.2 - Changed underlying data structure and added parallel processing
 # 0.0.3 - Added support for multiple input files
@@ -6,3 +6,4 @@ __version__ = "0.0.5"
 #         and the results object from the analysis to a msgpack binary file that
 #         is used for post-processing
 # 0.0.5 - Minor change to gradient calculations
+# 0.0.6 - Publishing to PyPi
