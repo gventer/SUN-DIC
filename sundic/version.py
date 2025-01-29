209fe697d0df5decdffcf5191fee18b289191a94
Gerhard Venter <gventer@sun.ac.za>
2025-01-29 14:26:43 +0200

    bumping version number

--- sundic/version.py
+++ sundic/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 # 0.0.1 - Initial release
 # 0.0.2 - Changed underlying data structure and added parallel processing
 # 0.0.3 - Added support for multiple input files
